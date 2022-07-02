3d23d7a8cb4004f1c40165a42bc02e7758028071
Luis David Trejos Rojas <108385721+WowNotSmart@users.noreply.github.com>
2022-07-02 09:44:22 -0500

    Added ledTicking.py as a plugAndRun file
    This is the first micro Python file added to the SNEIA's Dev Team repository.
    plugAndRun means that you can easily put this code in the main.py file in a Raspberry Pi Pico without worrying about the prototype related connections.
    The code uses the integrated Raspberry Pi Pico's LED and switch it ON and OFF depending on the specified parameters in the code.

---
+++ plugAndRun/ledTicking.py
@@ -0,0 +1,54 @@
+# SNEIA's Dev Team :)
+# Original code by: https://github.com/WowNotSmart
+# https://github.com/SNEIA-DEV-TEAM/raspberryPiPicoProjects/plugAndRun/ledTicking.py
+
+from machine import Pin
+import time, random
+
+raspberryLed = Pin(25, Pin.OUT)
+raspberryLedStatus = False
+
+# - - - - - IMPORTANT - - - - -
+
+# Change runningStatus's value
+# to True if you want to run the
+# code, change it back to False
+# if you don't want to run the
+# code. (Testing purposes)
+
+runningStatus = True
+
+# - - - - - - - - - - - - - - -
+
+timePeriod = 1          # How much time wait before the raspberryLedStatus change
+workMethod = "random"   # Possible values: ["random", "period"]
+
+def ledTicklingRandom():
+    global raspberryLed, raspberryLedStatus
+    if raspberryLedStatus == False:
+        raspberryLed.value(raspberryLedStatus)
+        time.sleep(random.uniform(0.01, 0.12))
+        raspberryLedStatus = True
+        
+    elif raspberryLedStatus == True:
+        raspberryLed.value(raspberryLedStatus)
+        time.sleep(random.uniform(0.01, 0.12))
+        raspberryLedStatus = False
+
+def ledTicklingPeriod():
+    global raspberryLed, raspberryLedStatus
+    if raspberryLedStatus == False:
+        raspberryLed.value(raspberryLedStatus)
+        time.sleep(timePeriod)
+        raspberryLedStatus = True
+        
+    elif raspberryLedStatus == True:
+        raspberryLed.value(raspberryLedStatus)
+        time.sleep(timePeriod)
+        raspberryLedStatus = False
+        
+while runningStatus == True:
+    if workMethod == "random":
+        ledTicklingRandom()
+    elif workMethod == "period":
+        ledTicklingPeriod()
